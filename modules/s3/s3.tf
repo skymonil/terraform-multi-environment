2abae3d457bfc96faa0ec06eafe5d2b2ca3fbcbe
Monil Parikh <158536159+skymonil@users.noreply.github.com>
2025-06-17 17:43:56 +0530

    Update s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -40,28 +40,6 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
 
 
 
-resource "aws_s3_bucket_policy" "allow_cf_only" {
-  bucket = aws_s3_bucket.frontend.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid: "AllowCloudFrontServicePrincipal",
-        Effect: "Allow",
-        Principal: {
-          Service: "cloudfront.amazonaws.com"
-        },
-        Action: "s3:GetObject",
-        Resource: "${aws_s3_bucket.frontend.arn}/*",
-        Condition: {
-          StringEquals: {
-            "AWS:SourceArn": aws_cloudfront_distribution.frontend_cf.arn
-          }
-        }
-      }
-    ]
-  })
-}
 
 
 # resource "local_file" "env_js" {
